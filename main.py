d3fc11074bc82d77543d8622caf6b5d479463b23
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-06-09 22:25:13 -0500

    initial api structure

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ import requests
 import os
 
 GOOGLE_API_KEY = os.environ.get('GOOGLE_PLACE_API')
+print(GOOGLE_API_KEY)
+
+
+LOCATION_ENDPOINT = "https://maps.googleapis.com/maps/api/geocode/json"
+
+location_parameters = {
+    "address": "3500 East Frank Phillips Blvd, Bartlesville, OK",
+    "key": GOOGLE_API_KEY
+
+}
+
+location_raw_data = requests.get(LOCATION_ENDPOINT, params=location_parameters)
+location_raw_data.raise_for_status()
+location_data = location_raw_data.json()
+print(location_data)
