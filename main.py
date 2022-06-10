6b971513249e7f94b547e9831c1a23c09b1d409a
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-06-09 22:45:40 -0500

    wrap code in function

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import requests
 import os
 
-GOOGLE_API_KEY = os.environ.get('GOOGLE_PLACE_API')
 
-user_location = input("Enter your address: \n")
+def user_geocode():
+
+    GOOGLE_API_KEY = os.environ.get('GOOGLE_PLACE_API')
+
+    user_location = input("Enter your address: \n")
+
+    LOCATION_ENDPOINT = "https://maps.googleapis.com/maps/api/geocode/json"
+
+    location_parameters = {
+        "address": user_location,
+        "key": GOOGLE_API_KEY
+
+    }
+
+    location_raw_data = requests.get(
+        LOCATION_ENDPOINT, params=location_parameters)
+    location_raw_data.raise_for_status()
+    location_data = location_raw_data.json()
+    print(location_data)
+    print("================================================================")
+
+    latitude = location_data['results'][0]['geometry']['location']['lat']
+    longitude = location_data['results'][0]['geometry']['location']['lng']
+    print(latitude)
+    print(longitude)
+    print("================================================================")
+    return (latitude, longitude)
 
 
-LOCATION_ENDPOINT = "https://maps.googleapis.com/maps/api/geocode/json"
-
-location_parameters = {
-    "address": user_location,
-    "key": GOOGLE_API_KEY
-
-}
-
-location_raw_data = requests.get(LOCATION_ENDPOINT, params=location_parameters)
-location_raw_data.raise_for_status()
-location_data = location_raw_data.json()
-print(location_data)
-print("================================================================")
-
-latitude = location_data['results'][0]['geometry']['location']['lat']
-longitude = location_data['results'][0]['geometry']['location']['lng']
-print(latitude)
-print(longitude)
+loc = user_geocode()
+print(loc)
