81b9dd7e78611eae4e2294b14cc8109e582f704f
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-06-11 23:49:06 -0500

    refactor hospital search function

--- main.py
+++ main.py
@@ -35,38 +35,18 @@ def user_geocode():
             return (latitude, longitude)
 
 
-# loc = user_geocode()
-# print(loc)
-
-
 def hospital_search():
-
-    # gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
-    # geocode_result = gmaps.geocode("4996 Princeton Drive, Bartlesville, OK")
-    # print(geocode_result)
 
     user_coordinates = user_geocode()
     user_latitude = user_coordinates[0]
     user_longitude = user_coordinates[1]
 
-    USER_ENDPOINT = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-
-    # search_parameters = {
-    #     "location": "36.153980%2C-95.992775",
-    #     "radius": 1500,
-    #     "type": "hospital",
-    #     "key": GOOGLE_API_KEY,
-    # }
-
     search_raw_data = requests.get(
         f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={user_latitude}%2C{user_longitude}&radius=1500&type=hospital&key={GOOGLE_API_KEY}")
-    # search_raw_data = requests.get(url=USER_ENDPOINT, params=search_parameters)
     search_raw_data.raise_for_status()
     search_data = search_raw_data.json()
 
-    # print(search_data)
     return search_data
 
 
-hosp_search = hospital_search()
-print(hosp_search)
+print(hospital_search())
